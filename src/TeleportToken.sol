ecac5fd26c1d06df9bac3fdd7e8455410b536a5e
0xJiro <jiro@sushi.com>
2023-10-11 18:06:15 -0400

    lay out for TeleportToken

---
+++ src/TeleportToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+contract TelepoortToken {
+
+    // duration it takes for the limits to fully replenish
+    // address of the factory which deployed this contract
+    // address of the lockbox contract
+    
+
+    // maps bridge address to bridge configurations
+
+    constructor(
+
+    ) {
+
+    }
+    
+
+    // write mint
+
+    // write burn
+
+    // write setLockbox
+
+    // write setLimits
+
+    // read mintingMaxLimitOf
+
+    // read burningMaxLimitOf
+
+    // read mintingCurrentLimitOf
+
+    // read burningCurrentLimitOf
+
+    // _useMinterLimits
+
+    // _useBunerLimits
+
+    // _changeMinterLimit
+
+    // _changeBurnerLimit
+
+    // _calculateNewCurrentLimit
+
+    // _getCurrentLimit
+
+    // _burnWithCaller
+
+    // _mintWithCaller
+
+    
+}
